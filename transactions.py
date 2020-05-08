c4bff02e3f2fa2740caf4f78fb2f4d9928d533d0
niranjay <niranjay.bhosale@gmail.com>
2020-05-08 23:26:19 +0530

    changed count_totals method to public

--- transactions.py
+++ transactions.py
@@ -1,79 +1,72 @@
-#Transactions.py
+#Transactions
 
 import signatures
 
-class transaction:
-    inputs = None
-    outputs = None
-    signatures = None
-    required = None
-
-
-    def __init__(self):
-        self.inputs = []
-        self.outputs = []
-        self.signatures = []
-        self.required = []
-
-
-    def add_input(self, from_address, amount):
-        self.inputs.append((from_address, amount))
-
-    
-    def add_output(self, to_address, amount):
-        self.outputs.append((to_address, amount))
-
-    
-    def add_required(self, address):
-        self.required.append(address)
-
-    
-    def sign(self, private):
-        message = self.__gather()
-        new_signature = signatures.signature(message, private)
-        self.signatures.append(new_signature)
-    
-    
-    def is_valid(self):
-        total_in = 0
-        total_out = 0
-        message = self.__gather()
-        for addr,amt in self.inputs:
-            found = False
-            for s in self.signatures:
-                if signatures.verify(message, s, addr):
-                    found = True
-            if amt < 0:
-                return False
-            if not found:
-                return False
-            total_in = total_in + amt
-        for addr in self.required:
-            found = False
-            for s in self.signatures:
-                if signatures.verify(message, s, addr):
-                    found = True
-            if not found:
-                return False
-        for addr,amt in self.outputs:
-            if amt < 0:
-                return False
-            total_out = total_out + amt
-        #if total_out > total_in:
-            #return False
-        return True
-
-
-    #private method: only used in this class therefore the double underscores "__" in prefix
-    def __gather(self):
-        data = []
-        data.append(self.inputs)
-        data.append(self.outputs)
-        data.append(self.required)
-        return data
-    
-    
-    def __repr__(self):
+class txn:
+	inputs = None
+	outputs = None
+	signatures = None
+	required = None 
+	
+	def __init__(self):
+		self.inputs = []
+		self.outputs = []
+		self.signatures = []
+		self.required = []
+	
+	def add_input(self, from_address, amount):
+		self.inputs.append((from_address, amount))
+		
+	def add_output(self, to_address, amount):
+		self.outputs.append((to_address, amount))
+		
+	def add_required(self, address):
+		self.required.append(address)
+		
+	def sign(self, private_key):
+		message = self.__gather()
+		new_signature = signatures.sign(message, private_key)
+		self.signatures.append(new_signature)
+		
+	def is_valid(self):
+		total_in = 0
+		total_out = 0
+		message = self.__gather()
+		for address, amount in self.inputs:
+			found = False
+			for s in self.signatures:
+				if signatures.verify(message, s, address):
+					found = True 
+			if not found:
+				return False
+			if amount < 0:
+				return False
+			total_in = total_in + amount 
+		for address in self.required:
+			found = False
+			for s in self.signatures:
+				if signatures.verify(message, s, address):
+					found = True 
+			if not found:
+				return False 
+		for address, amount in self.outputs:
+			if amount < 0:
+				return False
+			total_out = total_out + amount 
+			
+		#if total_in < total_out:
+#			return False 
+		
+		return True
+		
+	def __gather(self):
+		data = []
+		data.append(self.inputs)
+		data.append(self.outputs)
+		data.append(self.required)
+		return data
+		
+	def __repr__(self):
 		reprstr = "Inputs:\n"
 		for addr, amt in self.inputs:
 			reprstr = reprstr + str(amt) + " from " + str(addr) + "\n"
@@ -88,85 +81,97 @@ class transaction:
 			reprstr = reprstr + str(s) + "\n"
 		reprstr = reprstr + "End \n"
 		return reprstr
-    
+	    
+if __name__ == '__main__':
+	pr1, pu1 = signatures.generate_keys()
+	pr2, pu2 = signatures.generate_keys()
+	pr3, pu3 = signatures.generate_keys()
+	pr4, pu4 = signatures.generate_keys()
+	
+	# Regular Transaction 
+	tx1 = txn()
+	tx1.add_input(pu1, 1)
+	tx1.add_output(pu2, 1)
+	tx1.sign(pr1)
 
-if __name__ == "__main__":
-    pr1, pu1 = signatures.generate_keys()
-    pr2, pu2 = signatures.generate_keys()
-    pr3, pu3 = signatures.generate_keys()
-    pr4, pu4 = signatures.generate_keys()
+	# Transaction with 2 outputs 
+	tx2 = txn()
+	tx2.add_input(pu1, 2)
+	tx2.add_output(pu2, 1)
+	tx2.add_output(pu3, 1)
+	tx2.sign(pr1)
+	
+	
+	# Escrow Transaction  with input greater than output i.e transaction fee included 
+	tx3 = txn()
+	tx3.add_input(pu1, 1.2)
+	tx3.add_output(pu2, 1)
+	tx3.add_required(pu3)
+	tx3.sign(pr1)
+	tx3.sign(pr3) 
+	
+	# Checking for valid transactions 
+	for t in [tx1, tx2, tx3]:
+		if t.is_valid():
+			print("Success! Transaction is valid.")
+		else:
+			print("Error! Transaction is invalid.")
+			
+			
+	# Transaction with wrong signature
+	tx4 = txn()
+	tx4.add_input(pu1, 1)
+	tx4.add_output(pu2, 1)
+	tx4.sign(pr2)
+	
+	# Escrow Transaction not signed by the arbiter
+	tx5 = txn()
+	tx5.add_input(pu1, 1.2)
+	tx5.add_output(pu2, 1.1)
+	tx5.add_required(pu4)
+	tx5.sign(pr1)
+	
+	# Two input addresses but signed only by one
+	tx6 = txn()
+	tx6.add_input(pu3, 1)
+	tx6.add_input(pu4, 0.1)
+	tx6.add_output(pu2, 1.1)
+	tx6.sign(pr3)
+	
+	# Output exceeds the input : only for now but change this after implementing mining
+	tx7 = txn()
+	tx7.add_input(pu4, 1.2)
+	tx7.add_output(pu2, 1)
+	tx7.add_output(pu1, 2)
+	tx7.sign(pr4) 
+	
+	# Negative amount 
+	tx8 = txn()
+	tx8.add_input(pu1, -1)
+	tx8.add_output(pu2, -1)
+	tx8.sign(pr1)
+	
+	# Modified Transaction 
+	tx9 = txn()
+	tx9.add_input(pu1, 1)
+	tx9.add_output(pu2, 1)
+	tx9.sign(pr1)
+	tx9.outputs[0] = (pu3, 1)
+	
+			
+	# Checking for Bad transactions 
+	for t in [tx4 ,tx5, tx6, tx7, tx8, tx9]:
+		if t.is_valid():
+			print("Error! Bad Transaction couldn't detect.")
+		else:
+			print("Success! Bad Transaction detected")
 
-    # Checking correct signatures
-    tx1 = transaction()
-    tx1.add_input(pu1, 1)
-    tx1.add_output(pu2, 1)
-    tx1.sign(pr1)
-
-    tx2 = transaction()
-    tx2.add_input(pu1, 2)
-    tx2.add_output(pu2, 1)
-    tx2.add_output(pu3, 1)
-    tx2.sign(pr1)
-
-    tx3 = transaction()
-    tx3.add_input(pu3, 1.2)
-    tx3.add_output(pu1, 1.1)
-    tx3.add_required(pu4)
-    tx3.sign(pr3)
-    tx3.sign(pr4)
-
-    for t in [tx1, tx2, tx3]:
-        if t.is_valid():
-            print("Transaction is valid")
-        else:
-            print("Invalid Transaction")
 
 
-    # Checking wrong signatures
-    tx4 = transaction()
-    tx4.add_input(pu1, 1)
-    tx4.add_output(pu2, 1)
-    tx4.sign(pr2)
 
-    # Bad Escrow transaction (not signed by the arbiter)
-    tx5 = transaction()
-    tx5.add_input(pu3, 1.2)
-    tx5.add_output(pu1, 1.1)
-    tx5.add_required(pu4)
-    tx5.sign(pr3)
 
-    # Two input address but signed by one
-    tx6 = transaction()
-    tx6.add_input(pu3, 1)
-    tx6.add_input(pu4, 0.1)
-    tx6.add_output(pu1, 1.1)
-    tx6.sign(pr3)
 
-    # REMEMBER TO REMOVE THIS CONTRAIN AFTER MINING IMPLEMENTATION 
-    # Output exceeds input
-    tx7 = transaction()
-    tx7.add_input(pu4, 1.2)
-    tx7.add_output(pu1, 1)
-    tx7.add_output(pu2, 2)
-    tx7.sign(pr4)
 
-    # Negative amount as input
-    tx8 = transaction()
-    tx8.add_input(pu2, -1)
-    tx8.add_output(pu1, -1)
-    tx8.sign(pr2)
 
-    # Modified transaction
-    tx9 = transaction()
-    tx9.add_input(pu1, 1)
-    tx9.add_output(pu2, 1)
-    tx9.sign(pr1)
-    # output = [(pu2,1)]
-    # changed to = [(pu3,1)]
-    tx9.output_addresses[0] = (pu3,1)
-    
-    for t in [tx4, tx5, tx6, tx7, tx8, tx9]:
-        if t.is_valid():
-            print("Error, incorrect/bad transaction not detected")
-        else:
-            print("Great, incorrect/bad transaction detected")
+
+
