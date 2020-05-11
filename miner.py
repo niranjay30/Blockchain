31757135d303a234ec6340508bcabf720f899af2
niranjayTab <niranjay.bhosale@gmail.com>
2020-05-11 19:45:26 +0530

    combined both server and client functions and also creating a block is also implemented

--- miner.py
+++ miner.py
@@ -3,71 +3,148 @@
 import socket_utils
 import transactions
 import transaction_block
-import signatures
 
-wallets = ["localhost"]
+
+wallets = [("localhost", 5006)]
 txn_list = []
-head_blocks[None]
-
-def find_longest_blockchain():
-	longest = -1
-	long_head = None
-	for b in head_blocks:
-		current = b
-		this_len = 0
-		while current != None:
-			this_len = this_len + 1
-			current = current.previous_block
-		if this_len > longest:
-			long_head = b
-			longest = this_len
-	return long_head
+head_blocks = [None]
+break_now = False
+verbose = True
 
 
-def miner_server(my_ip, wallet_list, my_public):
-	server = socket_utils.new_server_connection(my_ip)
-	# Get 2 transactions from wallets
-	for i in range(10):
+def stop_all():
+	global break_now
+	break_now = True
+
+
+def miner_server(my_address):
+	global txn_list
+	global break_now
+	my_ip, my_port = my_address
+	server = socket_utils.new_server_connection(my_ip, my_port)
+	# Get transactions from wallets
+	while not break_now:
 		new_txn = socket_utils.recv_object(server)
 		if isinstance(new_txn, transactions.txn):
 			txn_list.append(new_txn)
-			print("Received transaction!")
-		if len(txn_list) >= 2:
-			break
+			if verbose:
+				print("Received transaction!")
+		#if len(txn_list) >= 2:
+		#	break
 	
-	# Add those transactions into a new block	
-	new_block = transaction_block.txn_block(find_longest_blockchain())
-	new_block.add_txn(txn_list[0])
-	new_block.add_txn(txn_list[1])
 	
-	# Compute and add mining reward
-	total_in, total_out = new_block.count_totals()
-	mine_reward = transactions.txn()
-	mine_reward.add_output(my_public, 25 + (total_in - total_out))
-	new_block.add_txn(mine_reward)
+def nonce_finder(wallet_list, miner_public):
+	global break_now
+	# Add those transactions into a new block
+	while not break_now:	
+		new_block = transaction_block.txn_block(transaction_block.find_longest_blockchain(head_blocks))
+		for txn in txn_list:
+			new_block.add_txn(txn)
+			#new_block.add_txn(txn_list[1])
 	
-	# Find the nonce
-	for i in range(10):
-		print("Finding nonce...")
-		new_block.find_nonce()
+		# Compute and add mining reward
+		total_in, total_out = new_block.count_totals()
+		mine_reward = transactions.txn()
+		mine_reward.add_output(miner_public, 25 + (total_in - total_out))
+		new_block.add_txn(mine_reward)
+		
+		# Find the nonce
+		if verbose:
+			print("Finding nonce...")
+		new_block.find_nonce(10000)
 		if new_block.good_nonce():
-			print("Good nonce found!")
-			break
+			if verbose:
+				print("Good nonce found!")
+			# Send new block
+			for ip_addr, port in wallet_list:
+				if verbose:
+					print("Sending to " + ip_addr + ":" + str(port))
+				socket_utils.send_object(ip_addr,new_block,5006)
+			head_blocks.remove(new_block.previous_block)
+			head_blocks.append(new_block)
+		
+			for txn in new_block.data:
+				if txn != mine_reward:
+					txn_list.remove(txn)
+	return True
+		
+if __name__ == "__main__":
 	
-	if not new_block.good_nonce():
-		print("Error! Couldn't find nonce.")
-		return False
-	# Send new block
-	for ip_addr in wallet_list:
-		print("Sending to " + ip_addr)
-		socket_utils.send_object(ip_addr,new_block,5006)
-	head_blocks.remove(new_block.previous_block)
-	head_blocks.append(new_block)
-	return False
-		
-		
-my_pr, my_pu = signatures.generate_keys()
-miner_server('localhost', wallets, my_pu)
+	import signatures
+	import threading
+	import time
+	
+	my_pr, my_pu = signatures.generate_keys()
+	t1 = threading.Thread(target=miner_server, args=(('localhost',5005),))
+	t2 = threading.Thread(target=nonce_finder, args=(wallets, my_pu))
+
+	server = socket_utils.new_server_connection('localhost', 5006)
+	
+	t1.start()
+	t2.start()
+	
+	pr1,pu1 = signatures.generate_keys()
+	pr2,pu2 = signatures.generate_keys()
+	pr3,pu3 = signatures.generate_keys()
+	
+	tx1 = transactions.txn()
+	tx2 = transactions.txn()
+	
+	tx1.add_input(pu1, 4.0)
+	tx1.add_input(pu2, 1.0)
+	tx1.add_output(pu3, 4.8)
+	tx2.add_input(pu3, 4.0)
+	tx2.add_output(pu2, 4.0)
+	tx2.add_required(pu1)
+	
+	tx1.sign(pr1)
+	tx1.sign(pr2)
+	tx2.sign(pr3)
+	tx2.sign(pr1)
+	
+	#print(tx1.is_valid())
+	#print(tx2.is_valid())
+	
+	try:
+		socket_utils.send_object('localhost', tx1)
+		print("Sent tx1.")
+		socket_utils.send_object('localhost', tx2)
+		print("Sent tx2.")
+	except:
+		print("Error! Connection unsuccessful")
+	
+
+	for i in range(40):
+		new_block = socket_utils.recv_object(server)
+		if new_block:
+			break		
+	
+	if new_block.is_valid():
+		print("Success! Block is valid.")
+	if new_block.good_nonce():
+		print("Success! Nonce is valid.")
+	
+	for txn in new_block.data:
+		try:
+			if txn.inputs[0][0] == pu1 and txn.inputs[0][1] == 4.0:
+				print("tx1 is present")
+		except:
+			pass
+		try:
+			if txn.inputs[0][0] == pu3 and txn.inputs[0][1] == 4.0:
+				print("tx2 is present")
+		except:
+			pass
+			
+	time.sleep(20)
+	break_now = True
+	time.sleep(2)
+	server.close()
+	
+	t1.join()
+	t2.join()
+	
+	print("Done!")
 	
 	
 
